5840a27af73f8b04f09b02b44d6d805ab9e38ebf
BrendonPierson <brendonpierson@gmail.com>
2015-09-04 12:00:59 -0500

    added fixed walls and spiders

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function preload() {
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.image('spider', 'assets/spider-icon.png');
 
     game.load.tilemap('map', 'NSS.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'simples_pimples.png');
@@ -17,6 +18,7 @@ var platforms;
 var cursors;
 
 var stars;
+var spiders;
 var score = 0;
 var scoreText;
 
@@ -26,61 +28,78 @@ var layer3;
 
 function create() {
 
-  game.stage.backgroundColor = '#787878';
-
-
-
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  A simple background for our game
     game.add.sprite(0, 0, 'sky');
+
+    //  Add json tilemap and create 3 layers 
     map = game.add.tilemap('map');
     map.addTilesetImage('NSS', 'tiles');
+
+
     layer1 = map.createLayer('backgroundLayer');
     layer2 = map.createLayer('blockedLayer');
-    layer3 = map.createLayer('objectLayer');
-    // blockedLayer
-    // objectLayer
+    layer3 = map.createLayer('objectsLayer');
+
+    map.setCollisionByExclusion([], true, 'blockedLayer');
+
+    layer1.resizeWorld();
+    layer2.resizeWorld();
+    // layer3.resizeWorld();
 
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+    //collision on blockedLayer
+    map.setCollisionBetween(5, 100, true, 'blockedLayer');
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
+    //resizes the game world to match the layer dimensions
+    // backgroundlayer.resizeWorld();
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-    layer2.body.immovable = true;
-
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
 
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    player = game.add.sprite(80, 600, 'dude');
 
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
 
     //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
+    // player.body.bounce.y = 0.2;
+    // player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
 
     //  Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+
+    // Make spiders
+
+    spiders = game.add.group();
+    spiders.enableBody = true;
+
+        for (var i = 0; i < 20; i++)
+    {
+        var y = Math.random();
+        var x = Math.random();
+
+        //  Create a star inside of the 'stars' group
+        var spider = spiders.create(game.world.randomX, game.world.randomY, 'spider');
+
+
+        //  Let gravity do its thing
+        // spider.body.gravity.y = 300;
+
+        //  This just gives each spider a slightly random bounce value
+        // spider.body.bounce.y = 0.7 + Math.random() * 0.2;
+
+        spider.body.collideWorldBounds = true;
+    }
+
+
+
+
+
 
     //  Finally some stars to collect
     stars = game.add.group();
@@ -108,20 +127,31 @@ function create() {
 
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
+
+    // Camera view that follows player
+    game.camera.follow(player);
     
 }
 
 function update() {
 
+    // Debug info
+    game.debug.cameraInfo(game.camera, 32, 32);
+    game.debug.spriteCoords(player, 32, 500);
+
     //  Collide the player and the stars with the platforms
     game.physics.arcade.collide(player, platforms);
     game.physics.arcade.collide(stars, platforms);
+    game.physics.arcade.collide(player, spiders);
+    game.physics.arcade.collide(spiders, player);    game.physics.arcade.collide(player, layer2);
 
     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
     game.physics.arcade.overlap(player, stars, collectStar, null, this);
 
     //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
+
 
 
 
@@ -139,6 +169,14 @@ function update() {
 
         player.animations.play('right');
     }
+    else if (cursors.up.isDown)
+    {
+      player.body.velocity.y = -150;
+    }
+    else if (cursors.down.isDown)
+    {
+      player.body.velocity.y = 150;
+    }
     else
     {
         //  Stand still
@@ -147,12 +185,6 @@ function update() {
         player.frame = 4;
     }
     
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -350;
-    }
-
 }
 
 function collectStar (player, star) {
@@ -165,3 +197,4 @@ function collectStar (player, star) {
     scoreText.text = 'Score: ' + score;
 
 }
+
