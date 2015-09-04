58b4add4ec3f4f6ea4b22d4b729032d3211eb9f5
Mary Kergosien <mary@kergosien.net>
2015-09-04 13:54:29 -0500

    new game map

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.onload = function() {
 
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+    var game = new Phaser.Game(1200, 800, Phaser.AUTO, '', { preload: preload, create: create });
 
     function preload () {
-
+        this.load.tilemap('map', 'NSS.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('logo', 'phaser.png');
 
     }
@@ -16,3 +16,5 @@ window.onload = function() {
     }
 
 };
+
+
