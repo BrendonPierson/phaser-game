70664049324ec8f59689c2dd61750aa814d58621
Deric Bunch <ddbunc2@gmail.com>
2015-09-04 20:07:12 -0500

    removing extra files and adding gameover screen

--- main.js
+++ main.js
@@ -2,106 +2,81 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
 
+  //Preload all images
   game.load.image('background','assets/debug-grid-1920x1920.png');
-  game.load.image('tootsieRoll', 'assets/tootsieRoll.png');
+  game.load.image('tootsieRoll', 'assets/tootsieRoll2.png');
   game.load.image('spider', 'assets/spider-icon.png');
-
   game.load.tilemap('map', 'NSS.json', null, Phaser.Tilemap.TILED_JSON);
   game.load.image('tiles', 'simples_pimples.png');
   game.load.image('Steve', 'assets/Steve.png');
   game.load.image('gameover', 'assets/Error1.png');
 }
 
+//Declare all variables
 var player;
 var cursors;
-
 var tootsieRolls;
 var spiders;
 var score = 0;
 var scoreText;
 var healthText;
 var gameover;
-
 var layer1;
 var layer2;
 
 function create() {
 
-  //  We're going to be using physics, so enable the Arcade Physics system
+  //  Enable the Arcade Physics system
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  //  Add json tilemap and create 3 layers 
+  //  Add json tilemap and create 2 layers 
   map = game.add.tilemap('map');
   map.addTilesetImage('NSS', 'tiles');
-
 
   layer1 = map.createLayer('backgroundLayer');
   layer2 = map.createLayer('blockedLayer');
 
+  //Set 'blackedLayer' so that player cannot pass through walls
   map.setCollisionByExclusion([], true, 'blockedLayer');
 
   layer1.resizeWorld();
   layer2.resizeWorld();
 
-  // The player and its settings
+  // Load Steve and his starting position
   player = game.add.sprite(80, 600, 'Steve');
 
   //  We need to enable physics on the player
   game.physics.arcade.enable(player);
 
-  //  Player physics properties. Give the little guy a slight bounce.
-  // player.body.bounce.y = 0.2;
-  // player.body.gravity.y = 300;
+  //  Player physics properties. 
   player.body.collideWorldBounds = true;
 
   // Give player health
   player.health = 3;
 
-
-
   // Make spiders
-
   spiders = game.add.group();
   spiders.enableBody = true;
 
-  for (var i = 0; i < 20; i++)
-  {
+  for (var i = 0; i < 20; i++) {
     var y = Math.random();
     var x = Math.random();
 
     var spider = spiders.create(game.world.randomX, game.world.randomY, 'spider');
 
-
-    //  Let gravity do its thing
-    // spider.body.gravity.y = 300;
-
     spider.body.collideWorldBounds = true;
   }
 
-
-
-
-
-
-  //  Finally some tootsoeRolls to collect
+  //  Add tootsieRolls to collect
   tootsieRolls = game.add.group();
 
-  //  We will enable physics for any tootsieRoll that is created in this group
+  //  Enable physics for any tootsieRoll that is created in this group
   tootsieRolls.enableBody = true;
 
-
-
-  //  Here we'll create 12 of them evenly spaced apart
-  for (var i = 0; i < 12; i++)
-  {
-    //  Create a star inside of the 'tootsieRolls' group
-    var tootsieRoll = tootsieRolls.create(i * 70, 0, 'tootsieRoll');
-
-    //  Let gravity do its thing
-    tootsieRoll.body.gravity.y = 300;
-
-    //  This just gives each tootsieRoll a slightly random bounce value
-    tootsieRoll.body.bounce.y = 0.7 + Math.random() * 0.2;
+  //  Create 12 tootsieRolls
+  for (var i = 0; i < 12; i++) {
+    //  Random placement of tootsieRolls group
+    var tootsieRoll = tootsieRolls.create(game.world.randomX, game.world.randomY, 'tootsieRoll');
   }
 
   //  The score
@@ -135,8 +110,7 @@ function update() {
   game.debug.spriteCoords(player, 32, 500);
 
 
-  //  Collide the player and the tootsieRoll with the platforms
-  game.physics.arcade.collide(tootsieRolls, layer2);    
+  //  Collide the player and the walls
   game.physics.arcade.collide(player, layer2);
 
   //  Checks to see if the player overlaps with any of the tootsieRolls, if he does call the collectTootsieRoll function
@@ -150,64 +124,61 @@ function update() {
   game.physics.arcade.overlap(player, spiders, spiderDamage, null, this);
 
 
-  if (cursors.left.isDown)
-  {
+  if (cursors.left.isDown) {
     //  Move to the left
     player.body.velocity.x = -150;
-
     player.animations.play('left');
   }
-  else if (cursors.right.isDown)
-  {
+  else if (cursors.right.isDown) {
     //  Move to the right
     player.body.velocity.x = 150;
-
     player.animations.play('right');
   }
-  else if (cursors.up.isDown)
-  {
+  else if (cursors.up.isDown) {
     player.body.velocity.y = -150;
   }
-  else if (cursors.down.isDown)
-  {
+  else if (cursors.down.isDown) {
     player.body.velocity.y = 150;
   }
-  else
-  {
+  else {
     //  Stand still
     player.animations.stop();
-
     player.frame = 4;
   }
 
   // Spider follow steve function
   spiders.forEach(followSteve, game.physics.arcade, false, 200);
-
 }
-
 
 function collectTootsieRoll (player, tootsieRoll) {
   
   // Removes the tootsieRoll from the screen
   tootsieRoll.kill();
-
   //  Add and update the score
   score += 10;
   scoreText.text = 'Hacker Points: ' + score;
+
+  //SET UP IF CONDITION FOR WIN!
 }
 
-function spiderDamage(player, spider){
+function spiderDamage(player, spider) {
   player.health -= 1;
   if (player.health <= 0) {
     player.kill();
-    var gameOverTitle = this.game.add.sprite(425,350,"gameover");
+
+    //Load gameover screen on player death
+    var gameOverTitle = this.game.add.sprite(425,450,"gameover");
     gameOverTitle.anchor.setTo(0.5,0.5);
+    //Temp reload page after 10 sec
+    setTimeout(function(){
+        location.reload();
+    }, 10000)
   }
   spider.kill();
   healthText.text = "Lives: " + player.health;
 }
 
-function followSteve(spider){
+function followSteve(spider) {
   if (player.body.x < spider.body.x){
     spider.body.velocity.x = -10;
   } else {
@@ -222,13 +193,10 @@ function followSteve(spider){
 
 function gofull() {
 
-  if (game.scale.isFullScreen)
-  {
+  if (game.scale.isFullScreen) {
     game.scale.stopFullScreen();
   }
-  else
-  {
+  else {
     game.scale.startFullScreen(false);
   }
-
 }
