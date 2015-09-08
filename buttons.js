1efd64e78bcd3d7481c7fc1af4d77b4417766fde
BrendonPierson <brendonpierson@gmail.com>
2015-09-08 14:38:06 -0500

    sorted scores for leaderboard

--- buttons.js
+++ buttons.js
@@ -6,8 +6,17 @@ $('#scores').click(function(){
   var fb = new Firebase("https://spider-game.firebaseio.com/scores");
   fb.orderByChild("user").on("value", function(snapshot) {
     var data = snapshot.val();
+    console.log("data", data);
+    var scoresArr = [];
+    for(obj in data){
+      data[obj].score = Number(data[obj].score);
+      scoresArr[scoresArr.length] = data[obj];
+    }
+    console.log("scoresArr", scoresArr);
+    var sortedScores = _.chain(scoresArr).sortBy('score').value();
+    console.log("sortedUniqueScores", sortedScores);
     var scoreObj = {
-      scores: data
+      scores: sortedScores.slice(0,10)
     };
     var source = $('#displayScores').html();
     var template = Handlebars.compile(source);
