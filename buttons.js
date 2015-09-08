8d63f2f93dbe8f459650c2b8cf9c146fc311ef18
BrendonPierson <brendonpierson@gmail.com>
2015-09-08 15:10:43 -0500

    added toggle

--- buttons.js
+++ buttons.js
@@ -6,21 +6,19 @@ $('#scores').click(function(){
   var fb = new Firebase("https://spider-game.firebaseio.com/scores");
   fb.orderByChild("user").on("value", function(snapshot) {
     var data = snapshot.val();
-    console.log("data", data);
     var scoresArr = [];
     for(obj in data){
       data[obj].score = Number(data[obj].score);
       scoresArr[scoresArr.length] = data[obj];
     }
-    console.log("scoresArr", scoresArr);
     var sortedScores = _.chain(scoresArr).sortBy('score').value();
-    console.log("sortedUniqueScores", sortedScores);
     var scoreObj = {
       scores: sortedScores.slice(0,10)
     };
     var source = $('#displayScores').html();
     var template = Handlebars.compile(source);
     var toDom = template(scoreObj);
-    $('#toDom').append(toDom);
+    $('#toDom').text(toDom);
+    $('#toDom').toggle();
   });
 });
