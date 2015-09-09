af4b95bd8390ce17be89c7b46e0df2308f984c8f
BrendonPierson <brendonpierson@gmail.com>
2015-09-09 12:02:23 -0500

    cleaned up folder structure

--- scripts/game_preload.js
+++ scripts/game_preload.js
@@ -9,8 +9,8 @@ tootsieRollSpider.Preload.prototype = {
     this.load.image('background','assets/debug-grid-1920x1920.png');
     this.load.image('tootsieRoll', 'assets/tootsieRoll2.png');
     this.load.image('spider', 'assets/spider-icon.png');
-    this.load.tilemap('map', 'NSS.json', null, Phaser.Tilemap.TILED_JSON);
-    this.load.image('tiles', 'simples_pimples.png');
+    this.load.tilemap('map', 'assets/NSS.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('tiles', 'assets/simples_pimples.png');
     this.load.image('Steve', 'assets/Steve.png');
     this.load.image('gameover', 'assets/Error1.png');
     this.load.image('goldenFan', 'assets/goldenFan.png');
